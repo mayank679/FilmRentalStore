27cd75c11a9ae3207ae96c24edac1a79c1cb8a9a
mayank679 <mayankraj6799@gmail.com>
2026-04-26 14:14:30 +0530

    Cleanup - removed redundant schema.sql

--- db/schema.sql
+++
@@ -1,7 +0,0 @@
--- Create Database
-DROP SCHEMA IF EXISTS film_rental;
-CREATE SCHEMA film_rental;
-USE film_rental;
-
--- The rest of the Sakila schema and data goes here. 
--- (I will include the full content in the actual file creation)
